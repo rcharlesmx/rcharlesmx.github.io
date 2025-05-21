0d5e98daf0f156f6054ce84a39b5ca4274206c58
rcharlesmx <87686850+rcharlesmx@users.noreply.github.com>
2025-05-21 16:26:11 -0600

    Webapp

---
+++ popes/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+    console.log("Service Worker instalado");
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    // Opcional: manejo offline aquí
+  });
+  
